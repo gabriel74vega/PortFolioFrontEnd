4f64ce7dddc951a649121d69be2d808a52aef004
gabriel74vega <gabriel74vega@gmail.com>
2023-05-26 23:20:10 -0300

    configuración de componentes

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -18,7 +16,7 @@ import { LogoutComponent } from './components/logout/logout.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
+    //HeaderComponent,
     BannerComponent,
     AboutComponent,
     ExperienceComponent,
@@ -30,7 +28,8 @@ import { LogoutComponent } from './components/logout/logout.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
