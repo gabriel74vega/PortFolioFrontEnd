b47b9ff361ec8c31469fdfc02051e8e13d7cf63f
gabriel74vega <gabriel74vega@gmail.com>
2023-05-26 23:26:11 -0300

    Archivos que se agrega al repositorio.

---
+++ PortfolioFrontEnd/src/app/components/header/header.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+}
+
+/*import {Component} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+*/
+/**
+ * @title Header
+ */
+/*@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+})
+export class HeaderComponent {}*/
