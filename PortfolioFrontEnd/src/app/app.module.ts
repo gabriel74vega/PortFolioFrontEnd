e7675eb9359c563fc9d5b4e95f8bacf8b8d2c307
gabriel74vega <gabriel74vega@gmail.com>
2023-06-15 19:08:50 -0300

    Componentes eduItems

--- PortfolioFrontEnd/src/app/app.module.ts
+++ PortfolioFrontEnd/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LoginComponent } from './components/login/login.component';
 import { LogoutComponent } from './components/logout/logout.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { ExpItemComponent } from './exp-item/exp-item.component';
+import { EduItemComponent } from './edu-item/edu-item.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { ExpItemComponent } from './exp-item/exp-item.component';
     ProjectsComponent,
     LoginComponent,
     LogoutComponent,
-    ExpItemComponent
+    ExpItemComponent,
+    EduItemComponent
   ],
   imports: [
     BrowserModule,
